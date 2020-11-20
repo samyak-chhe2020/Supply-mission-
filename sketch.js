ec3067a62481ad51efc8458b6de7158cacb9dd3e
samyak-chhe2020 <72869523+samyak-chhe2020@users.noreply.github.com>
2020-11-20 09:24:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,12 +31,12 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , (restitution:0.5, isStatic:true));
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, 650, width, 10 , (isStatic:true) );
  	World.add(world, ground);
 
 
@@ -58,6 +58,6 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-	Matter.Body.setStatic(false);	
+	Matter.Body.isStatic(false);	
 }
 }
